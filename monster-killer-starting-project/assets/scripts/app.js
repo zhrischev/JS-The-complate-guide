17651154825bfc63e9ea0153a4b02e3639f1b6ce
ZHrischev <zaprin.hrischev@abv.bg>
2022-06-23 00:15:24 +0300

    Adding strong attack and win status alert.

--- monster-killer-starting-project/assets/scripts/app.js
+++ monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,9 +8,38 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
+function attackMonster(mode){
+    let maxDamage;
+
+    if (mode === "ATTACK"){
+        maxDamage = ATTACK_VALUE;
+    }
+    else if (mode === "STRONG_ATTACK"){
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const playerDamage = dealMonsterDamage(maxDamage);
+    const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentMonsterHealth -= playerDamage;
+    currentPlayerHealth -= monsterDamage;
+
+    if (currentPlayerHealth > 0 && currentMonsterHealth <= 0){
+        alert("You won!");
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+        alert("You lost!");
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+        alert("You have a draw!");
+    }
+}
+
 function attackHandler(){
-    const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterHealth -= damage;
+    attackMonster("ATTACK")
+}
+
+function strongAttackHandler(){
+    attackMonster("STRONG_ATTACK")
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler)
